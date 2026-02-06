1ccfe82ba1d3cc8ede05b5896904594ec5cc0985
paulmrest <paulmrest@gmail.com>
2026-02-06 12:40:46 -0800

    Section 06 - .NET/C# Solutions Part 01

---
+++ Section06OptionalChallenges/Section06OptionalChallenges_DOTNET/Section06OptionalChallenges/FrequencyCounter_FindAllDuplicates.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+namespace Section06OptionalChallenges.FrequencyCounter;
+
+public partial class FrequencyCounter
+{
+  public static int[] FindAllDuplicates(int[] nums)
+  {
+    if (nums.Length <= 0)
+    {
+      return new int[]{};
+    }
+    //List<int> twice = new List<int>();
+    Dictionary<int, int> tracker = new Dictionary<int, int>();
+    foreach (int num in nums)
+    {
+      tracker[num] = tracker.ContainsKey(num) ? ++tracker[num] : 1;
+    }
+    return tracker.Where(kv => kv.Value > 1).Select(kv => kv.Key).ToArray();
+  }
+}
---
+++ Section06OptionalChallenges/Section06OptionalChallenges_DOTNET/Section06OptionalChallenges/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Section06OptionalChallenges.FrequencyCounter;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    // Console.WriteLine(FrequencyCounter.SameFrequency(182, 281));
+    // Console.WriteLine(FrequencyCounter.SameFrequency(31, 14));
+    // Console.WriteLine(FrequencyCounter.SameFrequency(3589578, 5879385));
+    // Console.WriteLine(FrequencyCounter.SameFrequency(22, 222));
+
+    // //true
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates('1', "b", 1, 'a'));
+
+    // //false
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates(1, 2, 3));
+
+    // //true
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates(1, 2, 2));
+
+    // //true
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates('a', 'b', 'c', 'a'));
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates("a", "b", "c", "a"));
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates("a", "b", "c", 'a'));
+
+    // //true
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates("1", "b", 1, "a"));
+
+    // //false
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates('a', 'b', 'c', 'A'));
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates("a", "b", "c", "A"));
+
+    // //true
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates('a', 'b', 'c', 'd', 'e', 'f', 'd'));
+    // Console.WriteLine(FrequencyCounter.AreThereDuplicates("a", "b", "c", "d", "e", "f", "d"));
+
+    // Console.WriteLine(FrequencyCounter.ConstructNote("aa", "abc"));
+    // Console.WriteLine(FrequencyCounter.ConstructNote("abc", "dcba"));
+    // Console.WriteLine(FrequencyCounter.ConstructNote("aabbcc", "bcabcaddff"));
+
+    Console.WriteLine(string.Join(" ", FrequencyCounter.FindAllDuplicates(new int[] {4,3,2,7,8,2,3,1})));
+    Console.WriteLine(string.Join(" ", FrequencyCounter.FindAllDuplicates(new int[] {4, 3, 2, 1, 0})));
+    Console.WriteLine(string.Join(" ", FrequencyCounter.FindAllDuplicates(new int[] {4, 3, 2, 1, 0, 1, 2, 3})));
+  }
+}
