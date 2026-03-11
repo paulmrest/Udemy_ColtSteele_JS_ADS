fae24a7e478677ecfb1ea98be008769e983c4d96
paulmrest <paulmrest@gmail.com>
2026-03-11 14:59:14 -0700

    Section 12 - JS Solutions

---
+++ Section12SelectionSort/selectionSort.js
@@ -0,0 +1,16 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([8, 2, 3, 1, 14, 5, 11]));
