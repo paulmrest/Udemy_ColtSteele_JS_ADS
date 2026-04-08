97dad6a75f4552af281c0e2f98248981c1a66c19
paulmrest <paulmrest@gmail.com>
2026-04-07 18:07:35 -0700

    Section 16 - Quick Sort

---
+++ Section16QuickSort/quickSortComparator.js
@@ -0,0 +1,37 @@
+function quickSort(arr, comparator = (a, b) => a - b, leftIndex = 0, rightIndex = arr.length - 1) {
+  if (leftIndex < rightIndex) {
+    let pivotIndex = pivot(arr, comparator, leftIndex, rightIndex);
+    quickSort(arr, comparator, leftIndex, pivotIndex - 1);
+    quickSort(arr, comparator, pivotIndex + 1, rightIndex);
+  }
+  return arr;
+}
+
+function pivot(arr, comparator = (a, b) => a - b, startIndex = 0, endIndex = arr.length - 1) {
+  let swapIndex = startIndex;
+  const pivotValue = arr[startIndex];
+  for (let i = startIndex + 1; i <= endIndex; i++) {
+    if (comparator(pivotValue, arr[i]) > 0) {
+      swapIndex++;
+      [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
+    }
+  }
+  [arr[startIndex], arr[swapIndex]] = [arr[swapIndex], arr[startIndex]];
+  return swapIndex;
+}
+
+console.log(quickSort([28, 41, 4, 11, 16, 1, 40, 14, 36, 37, 42, 18]));
+console.log(quickSort([26, 23, 27, 44, 17, 47, 39, 42, 43, 1]));
+console.log(quickSort([9, 4, 8, 2, 1, 5, 7, 6, 3]));
+
+var arr1 = [5, 4, 9, 10, 2, 20, 8, 7, 3];
+var arr2 = [8, 4, 2, 5, 0, 10, 11, 12, 13, 16];
+var arr3 = ["LilBub", "Garfield", "Heathcliff", "Blue", "Grumpy"];
+
+function strLength(a, b) {
+  return a.length - b.length
+}
+
+console.log(quickSort(arr1));
+console.log(quickSort(arr2));
+console.log(quickSort(arr3, strLength));
